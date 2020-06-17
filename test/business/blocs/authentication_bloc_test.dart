51f9efde7c70cbb9b1d6c0860b3829e2e30417c7
Marco Gelli <marcogelli1994@gmail.com>
2020-06-17 12:35:03 +0200

    AuthenticationBloc fully tested

--- test/business/blocs/authentication_bloc_test.dart
+++ test/business/blocs/authentication_bloc_test.dart
@@ -1,5 +1,6 @@
 import 'package:covtrack/business/blocs/authentication/authentication_bloc.dart';
 import 'package:covtrack/business/repositories/authentication/authentication_repository.dart';
+import 'package:covtrack/data/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:bloc_test/bloc_test.dart';
@@ -11,6 +12,10 @@ void main() {
   group('AuthenticationBloc', () {
     AuthenticationRepository authRepository;
     AuthenticationBloc authBloc;
+    User user = User(
+      uid: 'AbCd1234',
+      email: 'johndoe@mail.com',
+    );
 
     setUp(() {
       authRepository = MockAuthenticationRepository();
@@ -26,6 +31,49 @@ void main() {
 
     test('initial state is Uninitialized', () {
       expect(authBloc.initialState, Uninitialized());
+    });
+
+    group('AppStarted', () {
+      blocTest(
+        'emits [Authenticated] if the user is signed in',
+        build: () async {
+          when(authRepository.isSignedIn()).thenAnswer((_) async => true);
+          when(authRepository.getUser()).thenAnswer((_) async => user);
+          return authBloc;
+        },
+        act: (bloc) => bloc.add(AppStarted()),
+        expect: [Authenticated(user)],
+      );
+      blocTest(
+        'emits [Unauthenticated] if the user is not signed in',
+        build: () async {
+          when(authRepository.isSignedIn()).thenAnswer((_) async => false);
+          return authBloc;
+        },
+        act: (bloc) => bloc.add(AppStarted()),
+        expect: [Unauthenticated()],
+      );
+    });
+
+    group('LoggedIn', () {
+      blocTest(
+        'emits [Authenticated]',
+        build: () async {
+          when(authRepository.getUser()).thenAnswer((_) async => user);
+          return authBloc;
+        },
+        act: (bloc) => bloc.add(LoggedIn()),
+        expect: [Authenticated(user)],
+      );
+    });
+
+    group('LoggedOut', () {
+      blocTest(
+        'emits [Unauthenticated]',
+        build: () async => authBloc,
+        act: (bloc) => bloc.add(LoggedOut()),
+        expect: [Unauthenticated()],
+      );
     });
   });
 }
